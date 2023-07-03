dc71630450fdf84b1d25010d2c8531e390ec600c
djilit <ahmedoudjilit@gmail.com>
2023-07-03 03:40:15 +0200

    loginAdmin model aded

---
+++ backend/migrations/20230703013623-create-loginAdmin.js
@@ -0,0 +1,64 @@
+'use strict';
+const { DataTypes } = require("sequelize");
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.createTable("loginAdmin", {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      repExcepte: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false
+      },
+      reponse: {
+        type: DataTypes.JSON,
+        allowNull: true
+      },
+      Test: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+   
+      created_at: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+      updated_at: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.dropTable("loginAdmin");
+
+  }
+};
